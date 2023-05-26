2745db32699944fe3d6e62811ddbb1851632afb1
Mundeep <65218728+MundeepL@users.noreply.github.com>
2023-05-26 20:59:01 +0100

    Improved pifm-basic.sh
    Corrected grammatical errors and improved code-readability.

--- pifm-basic.sh
+++ pifm-basic.sh
@@ -3,6 +3,8 @@
 # This is a small script created by Mundeep Lamport as an alternative
 # to the GUI interface of PiFM, which is an FM transmitter app
 
+
+# Initialising and opening the install script by clearing the terminal to make install clutter-free
 clear
 echo " "
 echo "██████╗ ██╗    ███████╗███╗   ███╗     ██╗   ██████╗"
@@ -18,7 +20,7 @@ sleep 0.1
 echo "╚═╝     ╚═╝    ╚═╝     ╚═╝     ╚═╝     ╚═╝╚═╝╚══════╝"
 echo " "
 sleep 1
-echo "PiFM has been produced by Mundeep Lamport"
+echo "PiFM has been produced by MundeepL. Visit https://github.com/mundeepl"
 sleep 5
 clear
 echo "Welcome to PiFM. An FM-RDS capable transmitter."
@@ -31,6 +33,7 @@ echo "allowing you to start transmitting with just a few bits of key, user-custo
 sleep 5
 clear
 
+# Choosing transmission options by reading in vaariable values
 # Choose Frequency
 echo "Which frequency would you like to use? (85.5-108)"
 read frequency
@@ -48,7 +51,7 @@ echo "Which audio file should be transmitted? (file path with extension .wav .og
 read audio
 clear
 # Choose PI-Code
-echo "What PI-code would you like to use for the broadcast? (enter in hexidecimal)"
+echo "What PI-code would you like to use for the broadcast? (enter in hexadecimal)"
 read pi
 clear
 # Choose Program Type
@@ -56,7 +59,7 @@ echo "Choose the program type (https://bit.ly/2Q7JqhK has a list of these)"
 read pty
 clear
 # Choose Output Power (mpx)
-echo "Now choose the mpx (default 30 - audio starts breaking at ~50)"
+echo "Now choose the mpx variable (default 30 - audio starts breaking at ~50)"
 read mpx
 clear
 # Choose Output Strength
@@ -64,10 +67,12 @@ echo "Choose the power strength of the transmission. (max. 7)"
 read power
 clear
 # Choose Pre Emphasis
-echo "Choose the pre emphasis of the transmission. 'eu' or 'us'"
+echo "Choose the pre-emphasis of the transmission. 'eu' or 'us'"
 read pre
 clear
-# Cool Loading Bar - Realistically serves no purpose xD
+
+
+# Cool Loading Bar - Realistically serves no purpose
 echo -ne '[]                    []      (0%)\r'
 sleep 0.1
 echo -ne '[]#                   []      (5%)\r'
@@ -107,10 +112,13 @@ sleep 0.1
 echo -ne '[]##################  []      (90%)\r'
 sleep 0.1
 echo -ne '[]################### []      (95%)\r'
-sleep 0.1
+sleep 0.5
 echo -ne '[]####################[]      (100%)\r'
 sleep 1
 echo -ne 'Completed. Starting service PiFM.  \r'
 echo -ne '\n'
+
+
+# starting the PiFM software with the chosen variable flags
 cd /home/pi/PiFM/src
 sudo ./pifm --freq $frequency --ps $ps --rt "$rt" --audio $audio --pi $pi --pty $pty --mpx $mpx --power $power --preemph $pre
