a0be8bd650c30b4c6088f7175ff1c6fc764f661d
Вячеслав <130898539+vyacheslavsem@users.noreply.github.com>
2024-05-18 15:41:14 +0300

    Update setup.sh
    pi to kali

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ echo "Installing required tools" ; sleep 1
 sudo apt-get install libsndfile1-dev zenity -y
 clear
 echo "Changing directory --> src" ; sleep 1
-cd /home/pi/PiFM/src
+cd /home/kali/PiFM/src
 clear
 echo "Cleaning up" ; sleep 1
 make clean
@@ -27,26 +27,26 @@ echo "Making adjustments to --> /boot/config.txt" ; sleep 1
 echo "gpu_freq=250" | sudo tee -a /boot/config.txt > /dev/null
 clear
 echo "Adding software" ; sleep 1
-cd /home/pi/PiFM
+cd /home/kali/PiFM
 # Copy GUI Version
-sudo cp /home/pi/PiFM/pifm.sh /usr/local/bin
+sudo cp /home/kali/PiFM/pifm.sh /usr/local/bin
 sudo mv /usr/local/bin/pifm.sh /usr/local/bin/pifm
 # Copy Basic Version
-sudo cp /home/pi/PiFM/pifm-basic.sh /usr/local/bin
+sudo cp /home/kali/PiFM/pifm-basic.sh /usr/local/bin
 sudo mv /usr/local/bin/pifm-basic.sh /usr/local/bin/pifm-basic
 clear
 echo "Creating shortcuts" ; sleep 1
-sudo cp /home/pi/PiFM/src/PiFM.desktop /home/pi/Desktop
-sudo cp /home/pi/PiFM/src/PiFM.desktop /usr/share/applications
+sudo cp /home/kali/PiFM/src/PiFM.desktop /home/kali/Desktop
+sudo cp /home/kali/PiFM/src/PiFM.desktop /usr/share/applications
 echo "Changing filename/type" ; sleep 1
-sudo mv /home/pi/PiFM/src/pi_fm_adv /home/pi/PiFM/src/pifm
+sudo mv /home/kali/PiFM/src/pi_fm_adv /home/kali/PiFM/src/pifm
 clear
 echo "Patching xterm" ; sleep 1
 cd /usr/bin
 sudo cp lxterminal xterm
 clear
 echo "Assigning permissions" ; sleep 1
-sudo chmod +x /home/pi/PiFM/pifm
+sudo chmod +x /home/kali/PiFM/pifm
 sudo chmod +x /usr/local/bin/pifm
 sudo chmod +x /usr/local/bin/pifm-basic
 clear
